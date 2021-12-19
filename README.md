f361cd13aed842fdd48c20f33bed52c53578db10
Jeroen Overschie <jeroenoverschie@gmail.com>
2021-12-19 19:52:36 +0100

    Finishing touches ⭐️

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # performance-planner-assembler
 
-This tool helps you assembly the correct amount of pages for my _Performance planner_. It is both a journal and a planner and helps you bring structure to your daily routine. 
+This tool helps you assembly the correct pages for my _Performance planner_. It is both a journal and a planner and helps you bring structure to your daily routine. 
 
 ![performance planner example](./example_screenshot.png)
+
+After assembly, you can download the journal in PDF. Check out the [web app](https://dunnkers.com/performance-planner-assembly).
 
 ## About
 
--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import './App.css';
 import 'antd/dist/antd.css';
 import JournalMaker from './components/JournalMaker';
+import moment from 'moment';
 
 function App() {
+  const currentYear = moment().year();
+
   return (
     <div className="App">
-      <header className="App-header">
-        Performance Planner 
-        <small style={{color:"#ccc", display: "inline"}}>
-          Assembler ✨
+      <div className="App-content">
+        <header className="App-header">
+          Performance Planner 
+          <small style={{color:"#ccc", display: "inline"}}>
+            Assembler ✨
+          </small>
+        </header>
+        <JournalMaker />
+      </div>
+      <footer className='App-footer'>
+        <small>
+          © {currentYear}.
+          Built with ♥ by 
+          <a className="App-link" href="https://jeroenoverschie.nl/">Jeroen Overschie</a>
         </small>
-      </header>
-      <JournalMaker />
+      </footer>
     </div>
   );
 }
